02722a77ec86a2cec9c9f211a74ea4375bb7040d
Aaron Sproul <aaron@liveramp.com>
2020-01-08 15:26:29 -0800

    add config file back but check for existence first

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from configparser import ConfigParser, NoSectionError
 from pathlib import Path
 
 from exceptions import NoGitHubTokenException
@@ -8,6 +9,7 @@ from exceptions import NoGitHubTokenException
 APP_BASEDIR = Path(os.path.abspath(__file__)).parent
 APP_NAME = "liveramp/required-labels"
 
+CONFIG_FILENAME = "custom.conf"
 
 class ConfigException(Exception):
     pass
@@ -15,19 +17,36 @@ class ConfigException(Exception):
 
 def generate_config():
     config = {}
-    
-    config['required_any'] = os.environ.get('REQUIRED_LABELS_ANY', None)
-    config['required_all'] = os.environ.get('REQUIRED_LABELS_ALL', None)
-    config['banned'] = os.environ.get('BANNED_LABELS', None)
-    config['github_user'] = os.environ.get('GITHUB_USER', None)
-    config['github_pw'] = os.environ.get('GITHUB_PW', None)
-    config['github_token'] = os.environ.get('GITHUB_TOKEN', None)
-    config['github_status_text'] = os.environ.get('GITHUB_STATUS_TEXT', 'Label requirements not satisfied.')
-    config['github_status_url'] = os.environ.get('GITHUB_STATUS_URL', '')
+    conf = ConfigParser()
+
+    if os.path.exists(_get_config_file()):
+        conf.read(_get_config_file())
+        config['required_any'] = conf.get('Labels', 'required-labels-any')
+        config['required_all'] = conf.get('Labels', 'required-labels-all')
+        config['banned'] = conf.get('Labels', 'banned-labels')
+        config['github_user'] = conf.get('GitHub', 'user')
+        config['github_pw'] = conf.get('GitHub', 'password')
+        config['github_token'] = conf.get('GitHub', 'token')
+        config['github_status_text'] = conf.get('GitHub', 'status_check_text')
+        config['github_status_url'] = conf.get('GitHub', 'status_check_url')
+    else:
+        config['required_any'] = os.environ.get('REQUIRED_LABELS_ANY', None)
+        config['required_all'] = os.environ.get('REQUIRED_LABELS_ALL', None)
+        config['banned'] = os.environ.get('BANNED_LABELS', None)
+        config['github_user'] = os.environ.get('GITHUB_USER', None)
+        config['github_pw'] = os.environ.get('GITHUB_PW', None)
+        config['github_token'] = os.environ.get('GITHUB_TOKEN', None)
+        config['github_status_text'] = os.environ.get('GITHUB_STATUS_TEXT', 'Label requirements not satisfied.')
+        config['github_status_url'] = os.environ.get('GITHUB_STATUS_URL', '')
 
     for label in ['required_any', 'required_all', 'banned']:
         config[label] = config[label].split(',') if config[label] else None
     return config
+
+def _get_config_file():
+    if 'CONFIG_FILE' in os.environ:
+        return os.environ['CONFIG_FILE']
+    return os.path.join(APP_BASEDIR, CONFIG_FILENAME)
 
 CONFIG = generate_config()
 
@@ -54,7 +73,10 @@ if not UNIT_TESTING:
                                   CONFIG['github_token']])
     if not labels_configured or not credentials_configured:
         raise ConfigException(
-            "Please ensure that the environment variables are set.\n"
-            "such as REQUIRED_LABELS_ALL, REQUIRED_LABELS_ANY, or BANNED_LABELS along with "
-            "GITHUB_TOKEN or GITHUB_USER and GITHUB_PW"
+            "Please ensure your config file has a [Labels] and [Github] section.\n"
+            "Did you forget to create a configuration file?\n"
+            "You can do this by running\033[1m cp {0}.template {0} \033[0m\n"
+            "You can also add REQUIRED_LABELS_ALL, REQUIRED_LABELS_ANY, or BANNED_LABELS along with "
+            "GITHUB_TOKEN or GITHUB_USER and GITHUB_PW as environment variables"
+            "".format(CONFIG_FILENAME)
         )
--- tests/test_config.py
+++ tests/test_config.py
@@ -6,6 +6,21 @@ from config import generate_config, APP_BASEDIR
 
 
 class TestGenerateConfig(unittest.TestCase):
+    def test_it_source_config_from_environment_with_missing_conf(self):
+        with patch.dict(os.environ, {'CONFIG_FILE': '/conf/notexist',
+                                     'GITHUB_USER': 'ghuser'}):
+            config = generate_config()
+            assert config['github_user'] == 'ghuser'
+
+    def test_it_source_config_from_given_config_file(self):
+        fixture_config = os.path.join(APP_BASEDIR, 'tests', 'fixtures',
+                                      'custom.conf.test')
+        with patch.dict(os.environ, {'CONFIG_FILE': fixture_config}):
+            config = generate_config()
+            assert config['github_user'] == 'someuser'
+            assert config['github_status_text'] == "Label requirements not satisfied"
+            assert config['github_status_url'] == "https://somewhere_with_more_information.com"
+    
     def test_it_source_config_from_environment(self):
         with patch.dict(os.environ, {'GITHUB_USER': 'ghuser'}):
             config = generate_config()
