6a902cb0292b1bfad4090d1d10509740732d9326
Aaron Sproul <aaron@liveramp.com>
2020-01-09 11:31:31 -0800

    ensure config is reading from a non-existent filee

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,6 +6,23 @@ from config import generate_config, APP_BASEDIR
 
 
 class TestGenerateConfig(unittest.TestCase):
+    @patch('config.os.path.exists')
+    @patch('config.ConfigParser')
+    def test_it_reads_default_config_file(self, configparser, mock_os_path_exists):
+        mock_os_path_exists.return_value = True
+        generate_config()
+        configparser.return_value.read.assert_called_once_with(
+            os.path.join(APP_BASEDIR, 'custom.conf'))
+
+    @patch('config.ConfigParser')
+    def test_it_reads_given_config_file_from_environment_var(self,
+                                                             configparser):
+        fixture_config = os.path.join(APP_BASEDIR, 'tests', 'fixtures',
+                                      'custom.conf.test')
+        with patch.dict(os.environ, {'CONFIG_FILE': fixture_config}):
+            generate_config()
+            configparser.return_value.read.assert_called_once_with(fixture_config)
+    
     def test_it_source_config_from_environment_with_missing_conf(self):
         with patch.dict(os.environ, {'CONFIG_FILE': '/conf/notexist',
                                      'GITHUB_USER': 'ghuser'}):
